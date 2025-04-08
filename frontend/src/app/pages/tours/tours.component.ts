16689e75b504e20f5ccc409d74ce1e1487d48ca1
Aui <aui@gmail.com>
2025-04-08 23:07:49 +0500

    Divided parts  into pages,models and compoenents folders

---
+++ frontend/src/app/pages/tours/tours.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tours',
+  imports: [],
+  templateUrl: './tours.component.html',
+  styleUrl: './tours.component.css'
+})
+export class ToursComponent {
+
+}
