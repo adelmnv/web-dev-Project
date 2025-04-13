67041beb315aad3bc722ae9168775502a0e902e7
adelmnv <malgonussova@inbox.ru>
2025-04-13 19:45:08 +0500

    add corsheaders

--- backend/tour_back/tour_back/settings.py
+++ backend/tour_back/tour_back/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 # URL endpoint used to access media files in a browser
@@ -48,6 +49,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
 ]
 
 ROOT_URLCONF = 'tour_back.urls'
